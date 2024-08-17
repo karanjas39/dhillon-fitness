ff59132b965b92542c1c18ddc6ab029de4371600
karanjas39 <dhillonjaskaran4486@gmail.com>
2024-08-17 22:51:25 +0530

    logo done

--- frontend/src/components/Navbar/Logo.tsx
+++ frontend/src/components/Navbar/Logo.tsx
@@ -1,22 +1,16 @@
 import Image from "next/image";
-import LogoDark from "../../../public/logo-dark.png";
-import LogoLight from "../../../public/logo-light.png";
+import LogoImage from "/public/logo.png";
 import Link from "next/link";
-import { useTheme } from "next-themes";
 import { useSelector } from "react-redux";
 import { RootState } from "@/store";
 
 function Logo() {
-  const { resolvedTheme } = useTheme();
   const { token } = useSelector((state: RootState) => state.auth);
 
   return (
-    <Link href={token ? "/dashboard" : "/"} className="flex items-center gap-3">
-      {resolvedTheme === "light" ? (
-        <Image src={LogoLight} alt="Logo" width={250} />
-      ) : (
-        <Image src={LogoDark} alt="Logo" width={250} />
-      )}
+    <Link href={token ? "/dashboard" : ""} className="flex items-center">
+      <Image src={LogoImage} alt="Gym logo" width={50} />
+      <p className="font-bold text-2xl">DHILLON FITNESS</p>
     </Link>
   );
 }
