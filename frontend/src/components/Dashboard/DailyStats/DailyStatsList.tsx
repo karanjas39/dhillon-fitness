061e60c0c8674bda3dc754711767982059b3afb0
karanjas39 <dhillonjaskaran4486@gmail.com>
2024-08-28 23:20:13 +0530

    [fix] todays sales table overflow

--- frontend/src/components/Dashboard/DailyStats/DailyStatsList.tsx
+++ frontend/src/components/Dashboard/DailyStats/DailyStatsList.tsx
@@ -6,6 +6,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import {
   Table,
   TableBody,
@@ -37,38 +38,40 @@ function DailyStatsList({
           </DialogDescription>
         </DialogHeader>
         {dailyIncome.length ? (
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="text-left">Sr. No.</TableHead>
-                <TableHead className="text-center">Name</TableHead>
-                <TableHead className="text-center">Address</TableHead>
-                <TableHead className="text-right">Amount</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {dailyIncome.map((income, i) => (
-                <TableRow
-                  className="cursor-pointer"
-                  onClick={() =>
-                    router.push(`/customer/detail/${income.user.id}`)
-                  }
-                  key={income.user.id}
-                >
-                  <TableCell className="text-left">{i + 1}.</TableCell>
-                  <TableCell className="text-center capitalize">
-                    {income.user.name}
-                  </TableCell>
-                  <TableCell className="text-center capitalize">
-                    {income.user.address}
-                  </TableCell>
-                  <TableCell className="text-right">
-                    {Math.abs(income.paymentAmount)}
-                  </TableCell>
+          <ScrollArea className="max-h-[400px] overflow-y-auto pr-4">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="text-left">Sr. No.</TableHead>
+                  <TableHead className="text-center">Name</TableHead>
+                  <TableHead className="text-center">Address</TableHead>
+                  <TableHead className="text-right">Amount</TableHead>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+              </TableHeader>
+              <TableBody className="h-[200px] overflow-scroll">
+                {dailyIncome.map((income, i) => (
+                  <TableRow
+                    className="cursor-pointer"
+                    onClick={() =>
+                      router.push(`/customer/detail/${income.user.id}`)
+                    }
+                    key={income.user.id}
+                  >
+                    <TableCell className="text-left">{i + 1}.</TableCell>
+                    <TableCell className="text-center capitalize">
+                      {income.user.name}
+                    </TableCell>
+                    <TableCell className="text-center capitalize">
+                      {income.user.address}
+                    </TableCell>
+                    <TableCell className="text-right">
+                      {Math.abs(income.paymentAmount)}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </ScrollArea>
         ) : null}
       </DialogContent>
     </Dialog>
